ed9584f628fcb6d1849c40f6e53398303e63962d
Anshul Jain <hi@ajain.me>
2020-03-26 22:01:07 -0700

    initial commit

---
+++ lib/form/actions.rb
@@ -0,0 +1,8 @@
+require "form/actions/version"
+
+module Form
+  module Actions
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/form/actions/version.rb
@@ -0,0 +1,5 @@
+module Form
+  module Actions
+    VERSION = "0.1.0"
+  end
+end
